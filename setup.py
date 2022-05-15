40fa545b02e6c2cc17d17394700b56d128d1fa0f
philipp2310 <41761223+philipp2310@users.noreply.github.com>
2022-05-16 00:48:50 +0200

    install satellites via cli - V bump

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import find_packages, setup
 
 setup(
 	name='projectalice-cli',
-	version='1.3.1',
+	version='1.3.2',
 	long_description=Path('README.md').read_text(encoding='utf8'),
 	long_description_content_type='text/markdown',
 	python_requires='>=3.8',
